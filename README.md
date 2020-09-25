2937d19f6212b2e271d5e99a7dad0dbea6c94310
ShahmirFarooqui <shahmirfarooqui91@gmail.com>
2020-09-24 21:39:52 -0400

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# dddd # www
+# ReadMe
+
+# Github 
+
+
+shah22
+ 
+# Email
+
+
+shah22@gmail.com
+
+# Description
+
+
+This project was to create a ReadMe.md Dynamicly
+
+# Installation 
+
+
+npm install
+
+# Test 
+
+
+npm test 
+
+# Usage 
+
+
+Run node index,js 
+
+# Contributing 
+
+
+made by Shahmir F
+
+# License- BSD 3
+## Badges
+
+![](https://img.shields.io/badge/Language-NodeJs-green)
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,46 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} 
-# Github - 
- ${data.github}
+  return `# ${data.title}
+
+# Github 
+
+
+${data.github}
  
-# Github - 
- ${data.github}
-
-# Github - 
- ${data.github}
-
-# Github - 
- ${data.github}
+# Email
 
 
+${data.email}
+
+# Description
+
+
+${data.description}
+
+# Installation 
+
+
+${data.installation}
+
+# Test 
+
+
+${data.test} 
+
+# Usage 
+
+
+${data.usage}
+
+# Contributing 
+
+
+${data.contributing}
+
+# License- ${data.license}
+## Badges
+
+![](https://img.shields.io/badge/Language-NodeJs-green)
 
 `;
 }
